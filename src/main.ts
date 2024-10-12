c12828dba5704d60757170a148d8fb459f48b8c6
cezarfbc <cezarfbc@gmail.com>
2024-10-12 15:07:52 -0300

    feat(main): adding fastify configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const PORT = process.env.PORT;
+
+  await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
